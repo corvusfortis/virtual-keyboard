2c14455dc0fe77938720cce3dabc3541194eea13
corvusfortis <denisavidzba@mail.ru>
2023-05-01 16:06:46 +0300

    feat: add keyboard template

--- src/app.js
+++ src/app.js
@@ -3,12 +3,30 @@
   function createTextArea(){
 
     const container = document.createElement('div');
-    const textArea = document.createElement("textarea");
+    const textArea = document.createElement('textarea');
+    const keyboard = document.createElement('table');
+   
+ 
+
+    for (let i = 0; i < 6; i++){
+      const tableRow = document.createElement('tr');
+      for (let j = 0; j < 14; j++){
+        const tableCell = document.createElement('td');
+        tableCell.classList.add('vc__keyboard-cell');
+        tableRow.appendChild(tableCell);
+        
+      }
+
+      keyboard.appendChild(tableRow);
+
+    }
+
     container.classList.add('vc__container');
     textArea.classList.add('vc__textarea');
     textArea.cols = '120';
     textArea.rows = '15';
     container.appendChild(textArea);
+    container.appendChild(keyboard);
     document.body.appendChild(container);
     }
 
