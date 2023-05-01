f6482a003127c590c28e306ab734c39d25fb7e49
corvusfortis <denisavidzba@mail.ru>
2023-05-01 20:00:01 +0300

    feat: proper key span

--- src/app.js
+++ src/app.js
@@ -2,12 +2,11 @@
 
 
   const keyboardContent = [
-    [['Esc'], [''], ['F1'], ['F2'], ['F3'], ['F4'], ['F5'], ['F6'], ['F7'], ['F8'], ['F9'], ['F10'], ['F11'], ['F12']],
     [['`'], ['1'], ['2'], ['3'], ['4'], ['5'], ['6'], ['7'], ['8'], ['9'], ['0'], ['-'], ['='], ['Backspace']],
     [['Tab'], ['q'], ['w'], ['e'], ['r'], ['t'], ['y'], ['u'], ['i'], ['o'], ['p'], ['['], [']'], ['Enter']],
     [['Caps'], [''], ['a'], ['s'], ['d'], ['f'], ['g'], ['h'], ['j'], ['k'], ['l'], [';'], ['\''], ['\\']],
     [['Shift'], [''], ['z'], ['x'], ['c'], ['v'], ['b'], ['n'], ['m'], [','], ['.'], ['/'],  [''], ['Shift']],
-    [['Ctrl'], ['Win'], ['Alt'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], ['Alt'], ['Win'], ['Ctrl']]
+    [['Ctrl'], ['Win'], ['Alt'], [' '], [''], [''], [''], [''], [''], [''], [''], ['Alt'], ['Win'], ['Ctrl']]
   ]
 
   function createTextArea(){
@@ -18,14 +17,30 @@
    
  
 
-    for (let i = 0; i < 6; i++){
+    for (let i = 0; i < 5; i++){
       const tableRow = document.createElement('tr');
       for (let j = 0; j < 14; j++){
         const tableCell = document.createElement('td');
         tableCell.classList.add('vc__keyboard-cell');
         tableCell.textContent = keyboardContent[i][j];
-        tableRow.appendChild(tableCell);
-        
+        if(tableCell.textContent === 'Caps'){
+          tableCell.colSpan = '2';
+        }
+
+        switch(tableCell.textContent){
+          case 'Caps':
+            tableCell.colSpan = '2';
+            break;
+          case 'Shift':
+            tableCell.colSpan = '2';
+            break;
+          case ' ':
+            tableCell.colSpan = '8';
+            break;
+        }
+         if(tableCell.textContent.length !== 0){
+          tableRow.appendChild(tableCell);
+         }
       }
 
       keyboard.appendChild(tableRow);
@@ -54,7 +69,8 @@
       keys.forEach(e => {
         if(e.textContent === event.key.toLowerCase()){
           e.classList.add('vc__keyboard-cell_keydown');
-          document.querySelector('textarea').textContent = document.querySelector('textarea').textContent + e.textContent;
+            document.querySelector('textarea').textContent = document.querySelector('textarea').textContent + e.textContent;
+          
         }
       })
     }
